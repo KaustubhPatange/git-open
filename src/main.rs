4aa5112d02793b7d1292b75d4fb757f5712c3367
Kaustubh Patange <developerkp16@gmail.com>
2021-03-23 16:56:12 +0530

    [Fixed] -h command issue

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,16 @@
 mod back;
 
-use std::path::Path;
 use std::env::{args};
 use crate::back::command::Command;
 
 pub const NULL: &'static str = "null";
 
 fn main() {
-    let root = Path::new(".git");
-    if root.exists() && root.is_dir() {
-        let args : Vec<String> = args().collect();
-        match Command::parse_commands(&args) {
-            Ok(_) => {}
-            Err(e) => {
-                println!("- Error: {}", e)
-            }
+    let args : Vec<String> = args().collect();
+    match Command::parse_commands(&args) {
+        Ok(_) => {}
+        Err(e) => {
+            println!("- Error: {}", e)
         }
-    } else {
-        println!("The directory is not git repository")
     }
 }
